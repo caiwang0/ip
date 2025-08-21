77e06145d6b1a913536422dc848b3eb5dd174d28
caiwang0-admin <kelvincaiwang@gmail.com>
2025-08-22 00:32:27 +0800

    Finish Level-0

---
+++ src/main/java/Chip.java
@@ -0,0 +1,14 @@
+public class Chip { // <-- Remember to rename the file to Chip.java!
+
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+
+        System.out.println(horizontalLine);
+        System.out.println(" Hello! I'm Chip"); // <-- IMPORTANT: Change "Chip" to your bot's name
+        System.out.println(" What can I do for you?");
+        System.out.println(horizontalLine);
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
