0ceae758ae73dc8a0859538c30caa63ff428d3ba
caiwang0-admin <kelvincaiwang@gmail.com>
2025-08-22 12:19:12 +0800

    Finish Level-4

--- src/main/java/Chip.java
+++ src/main/java/Chip.java
@@ -20,6 +20,7 @@ public class Chip {
             String action = parts[0];
 
             System.out.println(horizontalLine);
+            Task newTask = null;
 
             if (action.equals("bye")) {
                 break;
@@ -41,13 +42,23 @@ public class Chip {
                 task.markAsNotDone();
                 System.out.println(" OK, I've marked this task as not done yet:");
                 System.out.println("   " + task);
-            } else {
-                Task newtask = new Task(command);
-                tasks.add(newtask);
-                System.out.println(horizontalLine);
-                System.out.println(" added: " + command);
-                System.out.println(horizontalLine);
+            } else if (action.equals("todo")) {
+                newTask = new Todo(parts[1]);
+            } else if (action.equals("deadline")) {
+                String[] deadlineParts = parts[1].split(" /by ");
+                newTask = new Deadline(deadlineParts[0], deadlineParts[1]);
+            } else if (action.equals("event")) {
+                String[] eventParts = parts[1].split(" /from ");
+                String[] timeParts = eventParts[1].split(" /to ");
+                newTask = new Event(eventParts[0], timeParts[0], timeParts[1]);
             }
+            if (newTask != null) {
+                tasks.add(newTask);
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + newTask);
+                System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+            }
+            System.out.println(horizontalLine);
         }
         System.out.println(horizontalLine);
         System.out.println(" Bye. Hope to see you again soon!");
