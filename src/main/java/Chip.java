816f5430ce7203cf699a8de0828aa04092eb2f31
caiwang0-admin <kelvincaiwang@gmail.com>
2025-08-22 11:09:30 +0800

    Finish Level-2

--- src/main/java/Chip.java
+++ src/main/java/Chip.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Chip {
 
@@ -10,15 +11,25 @@ public class Chip {
         System.out.println(" What can I do for you?");
         System.out.println(horizontalLine);
 
+        ArrayList<String> tasks = new ArrayList<>();
+
         Scanner in = new Scanner(System.in);
         while (true) {
             String command = in.nextLine();
             if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")) {
+                System.out.println(horizontalLine);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println(horizontalLine);
+            } else {
+                tasks.add(command);
+                System.out.println(horizontalLine);
+                System.out.println(" added: " + command);
+                System.out.println(horizontalLine);
             }
-            System.out.println(horizontalLine);
-            System.out.println(" " + command);
-            System.out.println(horizontalLine);
         }
         System.out.println(horizontalLine);
         System.out.println(" Bye. Hope to see you again soon!");
