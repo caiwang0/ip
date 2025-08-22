53aa6f00b45d87d743c797ca8a8e39b72297e666
caiwang0-admin <kelvincaiwang@gmail.com>
2025-08-22 10:50:44 +0800

    Finish Level-1

--- src/main/java/Chip.java
+++ src/main/java/Chip.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Chip {
 
     public static void main(String[] args) {
@@ -8,6 +10,17 @@ public class Chip {
         System.out.println(" What can I do for you?");
         System.out.println(horizontalLine);
 
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String command = in.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.println(horizontalLine);
+            System.out.println(" " + command);
+            System.out.println(horizontalLine);
+        }
+        System.out.println(horizontalLine);
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
     }
