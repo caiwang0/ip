61625a2b70c61eaf5f8a8b27580a98d18a957821
caiwang0-admin <kelvincaiwang@gmail.com>
2025-08-22 11:32:59 +0800

    Finish Level-3

--- src/main/java/Chip.java
+++ src/main/java/Chip.java
@@ -11,21 +11,39 @@ public class Chip {
         System.out.println(" What can I do for you?");
         System.out.println(horizontalLine);
 
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         Scanner in = new Scanner(System.in);
         while (true) {
             String command = in.nextLine();
-            if (command.equals("bye")) {
+            String[] parts = command.split(" ", 2);
+            String action = parts[0];
+
+            System.out.println(horizontalLine);
+
+            if (action.equals("bye")) {
                 break;
-            } else if (command.equals("list")) {
+            } else if (action.equals("list")) {
                 System.out.println(horizontalLine);
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println(" " + (i + 1) + ". " + tasks.get(i));
                 }
                 System.out.println(horizontalLine);
+            } else if (action.equals("mark")) {
+                int taskNumber = Integer.parseInt(parts[1]);
+                Task task = tasks.get(taskNumber - 1);
+                task.markAsDone();
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + task);
+            } else if (action.equals("unmark")) {
+                int taskNumber = Integer.parseInt(parts[1]);
+                Task task = tasks.get(taskNumber - 1);
+                task.markAsNotDone();
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + task);
             } else {
-                tasks.add(command);
+                Task newtask = new Task(command);
+                tasks.add(newtask);
                 System.out.println(horizontalLine);
                 System.out.println(" added: " + command);
                 System.out.println(horizontalLine);
